70930c69591197c642496b0cfd40b211691173f3
Samuel SIlva <samuelsilvawb@gmail.com>
2018-02-28 14:24:57 -0300

    feat : share quote on twitter
    - added function to share on twitter
    - linked event on click of image
    - changed cursor of clicable areas

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,9 @@
     const $ = require('./helpers.js');
     const http = require('./HttpModule.js');
     const $buttonNewQuote = $('#newQuote');
+    const $buttonShareTwitter = $('.quote__twitter > img');
     const END_POINT_QUOTES = 'https://talaikis.com/api/quotes/random/';
+    const END_POINT_SHARE_TWITTER = 'https://twitter.com/intent/tweet?text=';
     const handleQuote = function (obj) {
         const $elementQuote = $('.quote__text');
         $elementQuote.innerText = obj.quote;
@@ -11,5 +13,16 @@
     $buttonNewQuote.addEventListener('click', () => {
         http.get(END_POINT_QUOTES)
             .then(res => res.json().then(handleQuote));
+    });
+
+    $buttonShareTwitter.addEventListener('click', () => {
+        const textElementQuote = $('.quote__text').innerText;
+        if (textElementQuote) {
+            window.open(
+                END_POINT_SHARE_TWITTER + textElementQuote,
+                'Share on twitter',
+                'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=320, height=230'
+            );
+        }
     });
 }());
